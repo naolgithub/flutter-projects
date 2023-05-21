93bf7b294f2966f0b7bbdc62f4d751964091115a
naolgithub <naolcovered@gmail.com>
2023-05-21 16:38:40 +0300

    signing up users and logging in users are completed

--- zoom_clone/lib/main.dart
+++ zoom_clone/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/firebase_options.dart';
 
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'utils/colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -21,7 +27,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/login': (context) => const LoginScreen(),
-        '/home-screen': (context) => const HomeScreen(),
+        '/home': (context) => const HomeScreen(),
       },
       home: const LoginScreen(),
     );
