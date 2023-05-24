b4c3f3e6d7089b7ebdb191b68efea28a8783826a
naolgithub <naolcovered@gmail.com>
2023-05-24 11:18:44 +0300

    openAI API key is added

--- allen/lib/secrets.dart
+++ allen/lib/secrets.dart
@@ -1,1 +1,2 @@
-const openAIAPIKey = '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';
+// const openAIAPIKey = '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';
+const openAIAPIKey = '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';
