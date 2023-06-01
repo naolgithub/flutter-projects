0c1570516eecd3b70b7039aead23c7c8bc6de85b
naolgithub <naolcovered@gmail.com>
2023-06-01 14:49:50 +0300

    folder structure is added

---
+++ chatgpt_using_openai_api/lib/services/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetsManager {
+  static String imagePath = 'assets/images';
+  static String userName = "$imagePath/person.png";
+  static String botImage = "$imagePath/chat_logo.png";
+  static String openaiLogo = "$imagePath/openai_logo.png";
+}
