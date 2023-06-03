232a553531e56065ae3507a9fe71868a2cdde3df
naolgithub <naolcovered@gmail.com>
2023-06-03 22:16:55 +0300

    api key is changed

--- chatgpt_using_openai_api/lib/constants/api_consts.dart
+++ chatgpt_using_openai_api/lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = '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';
+String API_KEY = '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';
