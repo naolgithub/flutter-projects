ef0ffc0361cc5ff921c960badebb393eb9bc081a
naolgithub <naolcovered@gmail.com>
2023-06-03 22:11:45 +0300

    printing list of models using log() method

--- chatgpt_using_openai_api/lib/services/api_services.dart
+++ chatgpt_using_openai_api/lib/services/api_services.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:chatgpt_using_openai_api/constants/api_consts.dart';
+import 'package:chatgpt_using_openai_api/models/models_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
     try {
       var response = await http.get(
         Uri.parse('$BASE_URL/models'),
@@ -18,14 +20,25 @@ class ApiService {
       print('jsonResponse: $jsonResponse');
 
       if (jsonResponse['error'] != null) {
-        print("jsonResponse['error']${jsonResponse['error']["message"]}");
-        print("jsonResponse['error']${jsonResponse['error']["type"]}");
-        print("jsonResponse['error']${jsonResponse['error']["param"]}");
-        print("jsonResponse['error']${jsonResponse['error']["code"]}");
+        // print("jsonResponse['error']${jsonResponse['error']["message"]}");
+
+        // print("jsonResponse['error']${jsonResponse['error']["type"]}");
+        // print("jsonResponse['error']${jsonResponse['error']["param"]}");
+        // print("jsonResponse['error']${jsonResponse['error']["code"]}");
         throw HttpException(jsonResponse['error']["message"]);
       }
+      List temp = [];
+      for (var value in jsonResponse['data']) {
+        temp.add(value);
+        log('temp${value['id']}');
+        // print('temp${value['id']}');
+      }
+      return ModelsModel.modelFromSnapshot(temp);
     } catch (error) {
-      print('error$error');
+      // print('error$error');
+      log('error$error');
+      rethrow;
     }
+    // return [];
   }
 }
