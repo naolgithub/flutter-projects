6340c865782fdf444c253fd0b503014667a8e8f6
naolgithub <naolcovered@gmail.com>
2023-06-05 16:06:39 +0300

    api service is added

---
+++ bored_app_using_bored_api/lib/services/api_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:bored_app_using_bored_api/utils/api_constants.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/activity.dart';
+
+class ApiService {
+  static Future<Activity> getData() async {
+    try {
+      var response = await http.get(Uri.parse('$baseUrl/activity'));
+      var jsonResponse = jsonDecode(response.body);
+      // print('jsonResponse: $jsonResponse');
+      log('jsonResponse: $jsonResponse');
+      if (jsonResponse['error'] != null) {
+        // print("jsonResponse['error']${jsonResponse['error']}");
+        log("jsonResponse['error']${jsonResponse['error']}");
+        throw HttpException(jsonResponse['error']);
+      }
+      return Activity.fromJson(jsonResponse);
+    } catch (error) {
+      // print('error$error');
+      log('error$error');
+      rethrow;
+    }
+  }
+}
