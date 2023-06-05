21b8244c8c6045472869cef7a8b0bf1302995c34
naolgithub <naolcovered@gmail.com>
2023-06-05 12:12:11 +0300

    constants are modified

--- chatgpt_using_openai_api/lib/constants/api_consts.dart
+++ chatgpt_using_openai_api/lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = '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';
+String API_KEY = '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';
--- chatgpt_using_openai_api/lib/constants/constants.dart
+++ chatgpt_using_openai_api/lib/constants/constants.dart
@@ -1,56 +1,55 @@
-import 'package:chatgpt_using_openai_api/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 Color scaffoldBackgroundColor = const Color(0xFF343541);
 Color cardColor = const Color(0xFF444654);
 
-List<String> models = [
-  'Model1',
-  'Model2',
-  'Model3',
-  'Model4',
-  'Model5',
-  'Model6',
-];
+// List<String> models = [
+//   'Model1',
+//   'Model2',
+//   'Model3',
+//   'Model4',
+//   'Model5',
+//   'Model6',
+// ];
 
-List<DropdownMenuItem<String>>? get getModelsItem {
-  List<DropdownMenuItem<String>>? modelsItems =
-      List<DropdownMenuItem<String>>.generate(
-    models.length,
-    (index) => DropdownMenuItem(
-      value: models[index],
-      child: TextWidget(
-        label: models[index],
-        fontSize: 15,
-      ),
-    ),
-  );
-  return modelsItems;
-}
+// List<DropdownMenuItem<String>>? get getModelsItem {
+//   List<DropdownMenuItem<String>>? modelsItems =
+//       List<DropdownMenuItem<String>>.generate(
+//     models.length,
+//     (index) => DropdownMenuItem(
+//       value: models[index],
+//       child: TextWidget(
+//         label: models[index],
+//         fontSize: 15,
+//       ),
+//     ),
+//   );
+//   return modelsItems;
+// }
 
-final chatMessages = [
-  {
-    "msg": "Hello who are you?",
-    "chatIndex": 0,
-  },
-  {
-    "msg": "I am ChatGPT",
-    "chatIndex": 1,
-  },
-  {
-    "msg": "What is flutter?",
-    "chatIndex": 0,
-  },
-  {
-    "msg": "Flutter is a cross  platform framework for mobile development",
-    "chatIndex": 1,
-  },
-  {
-    "msg": "Do like flutter?",
-    "chatIndex": 0,
-  },
-  {
-    "msg": "I dont like, I love it.",
-    "chatIndex": 1,
-  },
-];
+// final chatMessages = [
+//   {
+//     "msg": "Hello who are you?",
+//     "chatIndex": 0,
+//   },
+//   {
+//     "msg": "I am ChatGPT",
+//     "chatIndex": 1,
+//   },
+//   {
+//     "msg": "What is flutter?",
+//     "chatIndex": 0,
+//   },
+//   {
+//     "msg": "Flutter is a cross  platform framework for mobile development",
+//     "chatIndex": 1,
+//   },
+//   {
+//     "msg": "Do like flutter?",
+//     "chatIndex": 0,
+//   },
+//   {
+//     "msg": "I dont like, I love it.",
+//     "chatIndex": 1,
+//   },
+// ];
