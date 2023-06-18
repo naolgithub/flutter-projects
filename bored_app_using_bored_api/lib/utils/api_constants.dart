477138e0165d92078311a3c33b83bef501654b52
naolgithub <naolcovered@gmail.com>
2023-06-18 12:16:32 +0300

    api_constants file is modified

--- bored_app_using_bored_api/lib/utils/api_constants.dart
+++ bored_app_using_bored_api/lib/utils/api_constants.dart
@@ -1,2 +1,20 @@
-String baseUrl = 'https://www.boredapi.com/api';
+import 'package:flutter/material.dart';
+
+String baseUrl = 'http://www.boredapi.com/api/activity?type=';
 String apiKey = '';
+
+class Constants {
+  final List<String> activityTypes = [
+    "education",
+    "recreational",
+    "social",
+    "diy",
+    "charity",
+    "cooking",
+    "relaxation",
+    "music",
+    "busywork",
+  ];
+
+  map(DropdownMenuItem<Object> Function(dynamic language) param0) {}
+}
