b71b3cf7591cba32030e65d2f09ca37f9e2f2b30
naolgithub <naolcovered@gmail.com>
2023-06-27 10:31:47 +0300

    api constants file is modified

--- chatgpt_using_openai_api/lib/constants/api_consts.dart
+++ chatgpt_using_openai_api/lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = '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';
+String API_KEY = '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';
